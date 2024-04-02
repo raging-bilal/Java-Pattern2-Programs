68e63dcf5c52518012cdbe90f0b12129177613ab
Khushnood Bilal <113290128+raging-bilal@users.noreply.github.com>
2024-04-02 08:49:53 +0530

    Add files via upload

---
+++ Console10.java
@@ -0,0 +1,13 @@
+public class Console10{
+    public static void main(String[] args){
+        int i ,j;    
+        int n = 6;    
+        for(i = n; i>0 ; i-- ){
+            for(j = 0; j<i ; j++){
+                System.out.print(j);
+            }    
+            System.out.println("");
+        }
+    }
+}        
+   
